60c02e277c74c57e56130e75ed28c2b7be50288e
dmuthuyia <dmuthuyia@gmail.com>
2019-04-20 02:47:02 +0300

    splash 1a

--- components/splash.js
+++ components/splash.js
@@ -1,25 +1,103 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Animated, Dimensions } from "react-native";
+import Assets from "../assets/assets";
 
-export default class Splash extends Component {
+const h = Dimensions.get("window").height;
+const height = h * 2;
+
+export default class SplashsScreen2 extends Component {
+  circle = new Animated.Value(0);
+  logo = new Animated.Value(0);
+  title = new Animated.Value(0);
+
+  componentDidMount() {
+    Animated.sequence([
+      Animated.timing(this.circle, {
+        toValue: 1,
+        duration: 2000
+      }),
+
+      Animated.timing(this.logo, {
+        toValue: 1,
+        duration: 1000
+      }),
+
+      Animated.timing(this.title, {
+        toValue: 1,
+        duration: 2000
+      })
+    ]).start();
+  }
+
   render() {
+    const translateY = this.circle.interpolate({
+      inputRange: [0, 1],
+      outputRange: [-height, 0]
+    });
+
+    const tranY = this.logo.interpolate({
+      inputRange: [0, 1],
+      outputRange: [h, 0]
+    });
+
+    const opacity = this.title.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, 1],
+      color: "green"
+    });
+
+    const opacity2 = this.title.interpolate({
+      inputRange: [0, 1],
+      outputRange: [1, 0]
+    });
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Welcome</Text>
+      <View style={styles.con}>
+        <Animated.Image
+          source={Assets.logo}
+          style={[styles.icon, { transform: [{ translateY: tranY }] }]}
+        />
+        <Animated.View
+          style={[styles.circle, { transform: [{ translateY }] }]}
+        />
+        <Animated.Text style={{ color: "#fff", fontSize: 50, opacity }}>
+          bookd
+        </Animated.Text>
+        <Animated.Text style={{ color: "#fff", fontSize: 14, opacity2 }}>
+          Hire my art
+        </Animated.Text>
+        <Animated.Text style={{ color: "#fff", fontSize: 12, opacity }}>
+          www.bookd.com
+        </Animated.Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "blue",
+  con: {
+    backgroundColor: "#fff",
     flex: 1,
     alignItems: "center",
     justifyContent: "center"
   },
-  title: {
+  titles: {
     fontWeight: "bold",
     fontSize: 18
+  },
+  circle: {
+    width: height,
+    height,
+    backgroundColor: "blue",
+    borderRadius: h,
+    position: "absolute",
+    zIndex: -1
+  },
+  icon: {
+    width: 100,
+    height: 80,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: "#fff"
   }
 });
